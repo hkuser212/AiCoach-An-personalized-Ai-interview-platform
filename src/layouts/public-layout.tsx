9b1956f4398a0addc36ad9d42846093787f9826b
hkuser212 <harshk210804@gmail.com>
2025-01-27 20:47:07 +0530

    Changes commited HomePage

--- src/layouts/public-layout.tsx
+++ src/layouts/public-layout.tsx
@@ -1,10 +1,12 @@
 import Footer from "@/components/footer"
 import Header from "@/components/header"
 import { Outlet } from "react-router-dom"
+import  AuthHandler from "@/handlers/auth-handler"
 const publiclayout = () => {
   return (
     <div className="w-full">
         <Header />
+        <AuthHandler/>
 
         <Outlet/>
 
