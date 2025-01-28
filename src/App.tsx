440771ccc358e2df0cc1c9e51322e2f8bc50f3eb
hkuser212 <harshk210804@gmail.com>
2025-01-28 17:44:22 +0530

    Final changes made

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,12 @@ import SignInPage from '@/routes/signin'
 import SignUpPage from '@/routes/signup'  
 import ProtectedRoutes from '@/layouts/protected-routes'
 import MainLayout from '@/routes/main-layout'
+import { Generate } from '@/components/generate'
+import Dashboard from '@/routes/dashboard'
+import {CreateEditPage} from '@/routes/create-edit-page'
+import {MockLoadPage} from '@/routes/mock-load-page'
+import {MockInterviewPage} from '@/routes/mock-interview-page'
+import { Feedback } from './routes/feedback'
 const App = () => {
   return (
  <Router>
@@ -19,7 +25,19 @@ const App = () => {
    <Route path='/signup/*' element={<SignUpPage/>} />
     </Route>
   
-  <Route element={<ProtectedRoutes><MainLayout/></ProtectedRoutes>}></Route>
+  <Route element={<ProtectedRoutes><MainLayout/></ProtectedRoutes>}>
+  <Route element={<Generate />}path="/generate">
+  <Route index element={<Dashboard/> }/>
+  <Route path =":interviewId" element={<CreateEditPage/>}/>
+  <Route path="interview/:interviewId" element={<MockLoadPage />} />
+  <Route
+              path="interview/:interviewId/start"
+              element={<MockInterviewPage />}
+            />
+   <Route path="feedback/:interviewId" element={<Feedback />} />
+  </Route>
+  
+  </Route>
   </Routes>
  </Router>
   )
